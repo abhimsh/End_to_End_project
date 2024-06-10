c419998aef1e35c441f015344d0b1f94e42fa81f
Abhilash Abhimsh <abhimshedu@gmail.com>
2024-06-10 11:38:58 +0530

    Added Dockerfile and yaml file for github action

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def predict_datapoint():
         return render_template("result.html", final_result=predicted_price)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
